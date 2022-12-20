3f0decbe20c763816071c85688c7372ba5d15f26
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2022-12-20 14:45:31 -0300

    chore: disable all prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,52 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  env: {
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+
+    camelcase: 'off',
+    'arrow-body-style': 'off',
+    'import/prefer-default-export': 'off',
+
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/style-prop-object': 'off',
+
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
