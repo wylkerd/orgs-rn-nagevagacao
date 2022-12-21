5dfe71f8d002f6cf5f55e3b11cb501d71bbfc54a
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2022-12-21 15:07:09 -0300

    chore: Produtor structure

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,13 +1,75 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
+import useTextos from '../../hooks/useTextos';
+
+import { StyleSheet, FlatList, View, Image, Text } from 'react-native'
+
+import Cesta from './componentes/Cesta';
+import Topo from '../../components/Topo'
+
+import topo from '../../assets/produtores/topo.png'
 
 export default function Produtor() {
   const route = useRoute();
+  const { tituloProdutor, tituloCestas } = useTextos();
+  const { nome, imagem, cestas } = route.params
+
+  const TopoLista = () => {
+    return (
+      <>
+        <Topo titulo={tituloProdutor} imagem={topo} altura={150} />
+        <View style={estilos.conteudo}>
+          <View style={estilos.logo}>
+            <Image source={imagem} style={estilos.produtorImage} />
+            <Text style={estilos.produtor}>{nome}</Text>
+          </View>
+
+          <Text style={estilos.cestas}>{tituloCestas}</Text>
+        </View>
+      </>
+    )
+  }
 
   return (
-    <View>
-      <Text>Produtor</Text>
-    </View>
+    <FlatList 
+      ListHeaderComponent={TopoLista}
+      data={cestas}
+      renderItem={({ item }) => <Cesta {...item} produtor={{ nome, imagem }} />}
+      style={estilos.lista}
+    />
   )
 }
+
+const estilos = StyleSheet.create({
+  lista: {
+      backgroundColor: '#ffffff',
+  },
+  conteudo: {
+      paddingHorizontal: 16,
+  },
+  logo: {
+      flexDirection: 'row',
+  },
+  produtorImage: {
+      width: 62,
+      height: 62,
+
+      marginTop: -23,
+
+      borderRadius: 6,
+  },
+  produtor: {
+      color: '#464646',
+      fontSize: 20,
+      lineHeight: 32,
+      fontWeight: 'bold',
+      marginLeft: 16,
+  },
+  cestas: {
+      color: '#464646',
+      fontSize: 20,
+      lineHeight: 32,
+      fontWeight: 'bold',
+      marginTop: 32,
+  }
+});
