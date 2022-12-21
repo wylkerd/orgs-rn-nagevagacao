5d54ec43f248b12ec95eaef4564922fec06bb380
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2022-12-21 14:31:37 -0300

    chore: sending data to the Produtor

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native'
 
 export default function Produtor() {
+  const route = useRoute();
+
   return (
     <View>
       <Text>Produtor</Text>
