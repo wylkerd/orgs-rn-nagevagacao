fbf3d15a71fbc7ffefb03a6bd26b51b7323b5111
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2022-12-21 22:59:24 -0300

    chore: styles and removing alert message #1

--- src/telas/Produtores/index.js
+++ src/telas/Produtores/index.js
@@ -11,16 +11,17 @@ export default function Produtores({ melhoresProdutores }) {
   const navigation = useNavigation();
   const route = useRoute();
 
-  const nomeCompra = route.params?.compra.nome;
-  // console.log(route.params)
-
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+  const { tituloProdutores, mensagemCompra } = useTextos();
 
+  const nomeCompra = route.params?.compra.nome;
+  const mensagemCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+
+  // !! negação da negação, para certificar que a variavél será um Boolean na verificação
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
-      <Text>{ nomeCompra }</Text>
+      { !!nomeCompra && <Text style={estilos.compra}>{ mensagemCompleta }</Text> }
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -51,5 +52,12 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
+  },
+  compra: {
+    backgroundColor: "#EAF5F3",
+    padding: 16,
+    color: "#464646",
+    fontSize: 16,
+    lineHeight: 26
   }
 })
